365e673602e5ef751f958fb2d94e7ac790010f5f
tere15 <kristiina.horneman@gmail.com>
2016-11-24 15:32:32 +0200

    v0.0.7 Forecabox

--- assets/index.js
+++ assets/index.js
@@ -3,7 +3,7 @@ var path = require("path");
 var bodyParser = require("body-parser");
 var http = require('http');
 
-var server = express();    
+var server = express();   
 
 server.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000);
 server.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
@@ -15,6 +15,7 @@ server.use('/app/components',express.static(path.join(__dirname, '../app/compone
 server.use('/assets/css',express.static(path.join(__dirname, '../assets/css')));
 server.use('/assets/bower_components',express.static(path.join(__dirname, '../assets/bower_components')));
 server.use('/assets',express.static(path.join(__dirname, '../assets')));
+
 
 http.createServer(server).listen(server.get('port') ,server.get('ip'), function() 
 {
